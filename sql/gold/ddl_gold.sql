1c4cfbc28154d9feaf66a4f040b507f78b9b5398
Pronoy-2003 <pronoypryunkushsonowal2003@gmail.com>
2026-08-31 00:08:04 +0530

    Update ddl_gold.sql

--- sql/gold/ddl_gold.sql
+++ sql/gold/ddl_gold.sql
@@ -3,14 +3,14 @@
 DDL Script: Create Gold Tables
 ===============================================================================
 Script Purpose:
-    This script creates tables for the Gold layer in the data warehouse. 
+    This script creates tables and views for the Gold layer in the data warehouse. 
     The Gold layer represents the final dimension and fact tables (Star Schema)
 
-    Each tables performs transformations and combines data from the Silver layer 
+    Each tables amd views performs transformations and combines data from the Silver layer 
     to produce a clean, enriched, and business-ready dataset.
 
 Usage:
-    - These tables can be queried directly for analytics and reporting.
+    - These tables and views can be queried directly for analytics and reporting.
 ===============================================================================
 */
 
