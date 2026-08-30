8267e50de5a05cfdd3e0b6891d1c4597919ad39e
Pronoy-2003 <pronoypryunkushsonowal2003@gmail.com>
2026-08-30 22:05:08 +0530

    Add files via upload

---
+++ src/config/cities.py
@@ -0,0 +1,121 @@
+CITIES = {
+    "Delhi": {
+        "country": "India",
+        "latitude": 28.6139,
+        "longitude": 77.2090
+    },
+
+    "Mumbai": {
+        "country": "India",
+        "latitude": 19.0760,
+        "longitude": 72.8777
+    },
+
+    "Kolkata": {
+        "country": "India",
+        "latitude": 22.5726,
+        "longitude": 88.3639
+    },
+
+    "Bengaluru": {
+        "country": "India",
+        "latitude": 12.9716,
+        "longitude": 77.5946
+    },
+
+    "Chennai": {
+        "country": "India",
+        "latitude": 13.0827,
+        "longitude": 80.2707
+    },
+
+    "Hyderabad": {
+        "country": "India",
+        "latitude": 17.3850,
+        "longitude": 78.4867
+    },
+
+    "Pune": {
+        "country": "India",
+        "latitude": 18.5204,
+        "longitude": 73.8567
+    },
+
+    "Ahmedabad": {
+        "country": "India",
+        "latitude": 23.0225,
+        "longitude": 72.5714
+    },
+
+    "Jaipur": {
+        "country": "India",
+        "latitude": 26.9124,
+        "longitude": 75.7873
+    },
+
+    "Guwahati": {
+        "country": "India",
+        "latitude": 26.1445,
+        "longitude": 91.7362
+    },
+
+    "Lucknow": {
+        "country": "India",
+        "latitude": 26.8467,
+        "longitude": 80.9462
+    },
+
+    "Patna": {
+        "country": "India",
+        "latitude": 25.5941,
+        "longitude": 85.1376
+    },
+
+    "Bhopal": {
+        "country": "India",
+        "latitude": 23.2599,
+        "longitude": 77.4126
+    },
+
+    "Bhubaneswar": {
+        "country": "India",
+        "latitude": 20.2961,
+        "longitude": 85.8245
+    },
+
+    "Kochi": {
+        "country": "India",
+        "latitude": 9.9312,
+        "longitude": 76.2673
+    },
+
+    "London": {
+        "country": "United Kingdom",
+        "latitude": 51.5074,
+        "longitude": -0.1278
+    },
+
+    "New York": {
+        "country": "United States",
+        "latitude": 40.7128,
+        "longitude": -74.0060
+    },
+
+    "Tokyo": {
+        "country": "Japan",
+        "latitude": 35.6762,
+        "longitude": 139.6503
+    },
+
+    "Singapore": {
+        "country": "Singapore",
+        "latitude": 1.3521,
+        "longitude": 103.8198
+    },
+
+    "Dubai": {
+        "country": "United Arab Emirates",
+        "latitude": 25.2048,
+        "longitude": 55.2708
+    }
+}
